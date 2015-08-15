879fcbcaead0aa4e809f70fa179b9175b0e83bf4
Kevin Scott <kevin@scottlabsllc.com>
2015-08-14 22:03:53 -0400

    get message listing going on

--- index.js
+++ index.js
@@ -4,10 +4,25 @@ var app = express();
 var text = require('./text');
 
 app.get('/', function (req, res) {
-    res.send('Sup');
+    text.send('8604608183', 'heyo').then(function(message) {
+        res.json(message);
+    }).fail(function(err) {
+        res.json(err);
+    });
 });
 
-var server = app.listen(3000, function () {
+app.get('/messages/:sid', function (req, res) {
+    text.get(req.params.sid).then(function(message) {
+        res.json(message);
+    }).fail(function(err) {
+        res.json(err);
+    });
+});
+
+app.get('/messages', function(req, res) {
+});
+
+var server = app.listen(5000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
--- text.js
+++ text.js
@@ -1,15 +1,56 @@
+var Q = require('q');
 // Twilio Credentials 
 var accountSid = 'ACe1033942b378b846fa69be0a58f04778'; 
 var authToken = '0b2a36fbb7cb1c6f558163f5fd61f081'; 
  
 //require the Twilio module and create a REST client 
 var client = require('twilio')(accountSid, authToken); 
+
+var from = "12039412722";
  
-client.messages.create({ 
-    to: "8604608183", 
-    from: "12039412722", 
-    body: "Hi there dude man",   
-}, function(err, message) { 
-    console.log(message.sid); 
-});
+function send(to, msg) {
+    var dfd = Q.defer();
+    client.messages.create({ 
+        to: to, 
+        from: from, 
+        body: msg
+    }, function(err, message) { 
+        if ( err ) {
+            dfd.reject(err);
+        } else {
+            dfd.resolve(message);
+        }
+    });
+    return dfd.promise;
+}
+
+function get(sid) {
+    var dfd = Q.defer();
+    client.messages(sid).get(function(err, message) {
+        if ( err ) {
+            dfd.reject(err);
+        } else {
+            dfd.resolve(message);
+        }
+    });
+    return dfd.promise;
+}
+
+function getAll() {
+
+    var dfd = Q.defer();
+    client.messages.list(function(err, message) {
+        if ( err ) {
+            dfd.reject(err);
+        } else {
+            dfd.resolve(message);
+        }
+    });
+    return dfd.promise;
+}
+
+module.exports = {
+    send: send,
+    get: get
+}
 
