7b3267f21881b69d1652b519dbcf8f295ba60324
schloo <lew.michelle@gmail.com>
2015-08-18 00:10:40 -0400

    supa fancy loading animation and input reset on submit

--- js/main.js
+++ js/main.js
@@ -9,19 +9,24 @@ $(document).ready(function(){
   // start chat animation
   loopChat();
 
-
-  // Send request to server
   $("#submit").click(function(){
+    // send request to server
     var val1= $("#tel-number").val();
     // $.post('http://192.168.1.140:5000/new', {number: val1 }, function(data) {console.log(data); } )
-    $('#submit').html('(spinning)');// execute spin
+    
+    // execute spinning animation
+    $('.submit-text').css('display','none');
+    $('.spinner').css('display','inline-block');
     setTimeout(function(){
-      $('#submit').html('Sent!'); //replace spin with sent
+      $('.submit-text').html('Sent!'); //replace spin with sent
+      $('.submit-text').css('display','inline-block');
+      $('.spinner').css('display','none'); // execute spin
       console.log('The number you submitted is: '+val1);
       setTimeout(function(){
-        $('#submit').html('Text another number!');
-      },1000); //'sent' duration
-    },2000); //spin duration  
+        $('#tel-number').val('');
+        $('.submit-text').html('Text another number');
+      },2000); //'sent' duration
+    },2500); //spin duration  
   });
 
   // Submit the form on click enter
