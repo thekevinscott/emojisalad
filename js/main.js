deca0131686184cbf9ef4ee6a288a16b94bece90
schloo <lew.michelle@gmail.com>
2015-08-17 22:40:10 -0400

    added faq section, made mobile tweaks, added basic button states

--- js/main.js
+++ js/main.js
@@ -2,14 +2,26 @@ $(document).ready(function(){
   // use masking plugin for input phone number
   $('#tel-number').mask('(000) 000-0000');
 
+  $('#wtf').click(function() {
+    $('.faq').slideToggle(300,'swing');
+  });
+
   // start chat animation
   loopChat();
-  
+
+
   // Send request to server
   $("#submit").click(function(){
     var val1= $("#tel-number").val();
-    console.log('The number you submitted is: '+val1);
     // $.post('http://192.168.1.140:5000/new', {number: val1 }, function(data) {console.log(data); } )
+    $('#submit').html('(spinning)');// execute spin
+    setTimeout(function(){
+      $('#submit').html('Sent!'); //replace spin with sent
+      console.log('The number you submitted is: '+val1);
+      setTimeout(function(){
+        $('#submit').html('Text another number!');
+      },1000); //'sent' duration
+    },2000); //spin duration  
   });
 
   // Submit the form on click enter
