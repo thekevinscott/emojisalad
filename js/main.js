ff4937bba4e3aa20a6ef5f5fd155a0ad1b0c68d6
schloo <lew.michelle@gmail.com>
2015-08-21 21:16:35 -0400

    form field validation and interactive tweaks

--- js/main.js
+++ js/main.js
@@ -1,32 +1,23 @@
+var defaultMsg = 'no spam, we promise.<br>standard messaging rates apply.';
+var errorMsg = 'error: not a valid phone number.<br>please try again.';
+
 $(document).ready(function(){
-  // use masking plugin for input phone number
-  $('#tel-number').mask('(000) 000-0000');
-
-  $('#wtf').click(function() {
-    $('.faq').slideToggle(300,'swing');
-  });
-
-  // start chat animation
-  loopChat();
+  $('#tel-messaging').html(defaultMsg);
+  $('#tel-number').mask('(000) 000-0000'); // use masking plugin for phone number
 
   $("#submit").click(function(){
-    // send request to server
-    var val1= $("#tel-number").val();
-    // $.post('http://192.168.1.140:5000/new', {number: val1 }, function(data) {console.log(data); } )
-    
-    // execute spinning animation
-    $('.submit-text').css('display','none');
-    $('.spinner').css('display','inline-block');
-    setTimeout(function(){
-      $('.submit-text').html('Sent!'); //replace spin with sent
-      $('.submit-text').css('display','inline-block');
-      $('.spinner').css('display','none'); // execute spin
-      console.log('The number you submitted is: '+val1);
-      setTimeout(function(){
-        $('#tel-number').val('');
-        $('.submit-text').html('Text another number');
-      },2000); //'sent' duration
-    },2500); //spin duration  
+    var enteredPhoneNum= $("#tel-number").val();
+    // validate phone number
+    if (enteredPhoneNum.length != 14) {
+      // throw error
+      $('#tel-messaging').addClass('error');
+      $('#tel-messaging').html(errorMsg);
+      $('#tel-number').focus();
+    } else {
+      $('#tel-messaging').removeClass('error');
+      $('#tel-messaging').html(defaultMsg);
+      submitPhoneNumber(enteredPhoneNum);
+    }
   });
 
   // Submit the form on click enter
@@ -37,5 +28,34 @@ $(document).ready(function(){
       return false;
     }
   });
-	
+
+  $('#wtf').click(function() {
+    $('.faq').slideToggle(300,'swing');
+  });
+
+  // start chat animation
+  loopChat();
 });
+
+function submitPhoneNumber(phoneNum) {
+  // send request to server
+  // $.post('http://192.168.1.140:5000/new', {number: phoneNum }, function(data) {console.log(data); } )
+  console.log('The number you submitted is: ' + phoneNum);
+    
+  // execute spinning animation
+  $('.submit-text').css('display','none');
+  $('.spinner').css('display','inline-block');
+  setTimeout(function(){
+    // replace spin with sent
+    $('.submit-text').html('Text sent!');
+    $('#submit').addClass('highlight');
+    $('.submit-text').css('display','inline-block');
+    $('.spinner').css('display','none');
+    setTimeout(function(){
+      // replace sent with text another number
+      $('#tel-number').val('');
+      $('#submit').removeClass('highlight');
+      $('.submit-text').html('Text another number');
+    },1500); //'sent' duration
+  },2500); //spin duration  
+}
