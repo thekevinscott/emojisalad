522c8db18e2cb8fd08c1d0e1b7e4a0762ae8d970
Kevin Scott <kevin@scottlabsllc.com>
2015-09-01 11:22:33 -0400

    mark confirmation messages as case insensitive

--- api/routes/users/confirm.js
+++ api/routes/users/confirm.js
@@ -2,7 +2,7 @@ var User = require('../../models/user');
 var Message = require('../../models/message');
 
 module.exports = function(user, input) {
-  if ( /^yes|^yeah|^yea|^y$/.test(input) ) {
+  if ( /^yes|^yeah|^yea|^y$/i.test(input) ) {
     return Message.get('intro_2').then(function(message) {
       message.type = 'respond';
       User.update(user, {
