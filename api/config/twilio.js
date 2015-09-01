07c1bf74723b48d4ea1084d1baf1b9f508f08ffe
Kevin Scott <kevin@scottlabsllc.com>
2015-08-31 23:39:28 -0400

    allow switching from number based on environment

--- api/config/twilio.js
+++ api/config/twilio.js
@@ -1,6 +1,12 @@
 var accountSid = 'ACf2076b907d44abdd8dc8d262ff941ee4'; 
 var authToken = '0d7b1b491ca038d4ff4fdf674cd46aa1'; 
-var from = "18603814348";
+
+
+if ( process.env.ENVIRONMENT === 'production' ) {
+  var from = "12013409832";
+} else {
+  var from = "18603814348";
+}
 
 module.exports = {
   accountSid: accountSid,
