17764d0ea39957a291058c7c19f77d28720aeef2
Kevin Scott <kevin@scottlabsllc.com>
2015-09-02 19:03:04 -0400

    add mixpanel

--- api/tracking.js
+++ api/tracking.js
@@ -1,4 +1,7 @@
 var ua = require('universal-analytics');
+var Mixpanel = require('mixpanel');
+
+var mixpanel = Mixpanel.init('95ea55113750200c91effcaade70941b');
 
 module.exports = function(state, user_id, input) {
   if ( 1 || process.env.ENVIRONMENT === 'production' ) {
@@ -12,6 +15,12 @@ module.exports = function(state, user_id, input) {
       ea: state,
       ev: input
     }).send();
+
+    mixpanel.track("Twilio Request", {
+      user_id: user_id,
+      state: state,
+      input: input
+    });
 
   }
 }
