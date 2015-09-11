dbeb8b9e7a3669d0caa92f8658597fa593b64e78
Kevin Scott <kevin@scottlabsllc.com>
2015-09-11 00:20:57 -0400

    this might be a fix for the randomization thing

--- api/models/game.js
+++ api/models/game.js
@@ -191,6 +191,7 @@ var Game = {
     var query = squel
                 .select()
                 .field('g.id')
+                .field('g.random')
                 .field('s.state')
                 .from('games', 'g')
                 .left_join('game_participants', 'p', 'p.game_id = g.id')
