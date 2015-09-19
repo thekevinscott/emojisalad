92dca604c39bed6af347b709c299d1056ed381b1
Kevin Scott <kevin@scottlabsllc.com>
2015-09-19 13:39:40 -0400

    update test and dev goog analytics

--- config/google-analytics.js
+++ config/google-analytics.js
@@ -1,5 +1,7 @@
 module.exports = {
   production: 'UA-66714330-1',
+  test: 'UA-66714330-2',
+  development: 'UA-66714330-2',
   //production: 'UA-67117728-1',
   //test: 'UA-67117728-2',
   //development: 'UA-67117728-2'
