f38b1e689aee8d70ec49b61614db1cc9a06ba7c1
Kevin Scott <kevin@scottlabsllc.com>
2015-09-30 12:26:29 -0400

    only select participants who are not archived

--- api/models/game.js
+++ api/models/game.js
@@ -147,6 +147,7 @@ let Game = {
                 .left_join('platforms', 'p', 'p.id = u.platform_id')
                 .where('g.id=?', game.id)
                 .where('g.archived=0')
+                .where('u.archived=0')
                 .order('u.id');
 
     return db.query(query).then(function(users) {
@@ -188,6 +189,7 @@ let Game = {
                 .left_join('game_participants', 'p', 'p.game_id = g.id')
                 .left_join('game_states', 's', 's.id = g.state_id')
                 .where('g.archived=0')
+                .where('u.archived=0')
                 .order('g.created', false)
                 .limit(1);
 
