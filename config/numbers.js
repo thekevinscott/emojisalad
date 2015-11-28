879311221564ed5811f607d597bb15987d6109fc
Kevin Scott <kevin@scottlabsllc.com>
2015-11-28 17:23:40 -0500

    Add twilio numbers

---
+++ config/numbers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var game_numbers = [
+  '+12013409832',
+  '+14692426535',
+];
+
+module.exports = game_numbers;
+module.exports.getDefault = function() {
+  return game_numbers[0];
+};
