21315b9f2b1f95a3222e810af60e1fff387374a9
Kevin Scott <kevin@scottlabsllc.com>
2015-12-08 14:12:16 -0500

    Update typo

--- api/cron/index.js
+++ api/cron/index.js
@@ -6,7 +6,7 @@ const Promise = require('bluebird');
 
 const time = '00 00 12 * * *';
 const checkGames = require('./checkGames');
-let job = Promise.coroutine(function* {
+let job = Promise.coroutine(function* () {
   console.log('The time is 12 o clock');
   let response = yield checkGames();
   console.log('response', response);
