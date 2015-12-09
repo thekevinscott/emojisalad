46ea00958121bcb6b819fcfd8e7111dd8a810a42
Kevin Scott <kevin@scottlabsllc.com>
2015-12-09 17:05:54 -0500

    not every second ahhhhhhh

--- api/cron/index.js
+++ api/cron/index.js
@@ -4,7 +4,7 @@ const Message = require('models/message');
 const Twilio = require('models/twilio');
 const Promise = require('bluebird');
 
-const time = '* * * * * *';
+const time = '00 * * * * *';
 //const time = '00 00 12 * * *';
 const checkGames = require('./checkGames');
 let job = Promise.coroutine(function* () {
