a48e86a46c03a426bf89b534266e256fc74fd32b
Kevin Scott <kevin@scottlabsllc.com>
2015-12-30 17:59:46 -0500

    update sms queue to move code to scaffolding

--- smsqueue/index.js
+++ smsqueue/index.js
@@ -4,30 +4,5 @@ require('babel/register');
 // set require path
 require('app-module-path').addPath(__dirname);
 
-const pmx = require('pmx');
-let DEBUG = (process.env.DEBUG !== undefined) ? process.env.DEBUG : true;
-console.debug = function() {
-  if ( DEBUG === true || DEBUG === 'true' ) {
-    let args = Array.prototype.slice.call(arguments);
-    args.unshift(new Date());
-    console.log.apply(null, args);
-  }
-};
-let consoleError = console.error;
-console.error = function() {
-  let args = Array.prototype.slice.call(arguments);
-  args.unshift(new Date());
-  consoleError.apply(null, args);
-};
-pmx.action('debug:on', function(reply) {
-  console.log('debug is on');
-  DEBUG = true;
-  reply({DEBUG : DEBUG});
-});
-pmx.action('debug:off', function(reply) {
-  console.log('debug is off');
-  DEBUG = false;
-  reply({DEBUG : DEBUG});
-});
-
+require('./scaffolding'); 
 require('./server'); 
