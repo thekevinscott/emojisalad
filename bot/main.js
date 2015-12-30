d4b29d0d2d8dd4ef16ababef298fc0e3658d841e
Kevin Scott <kevin@scottlabsllc.com>
2015-12-30 09:09:29 -0500

    concatenate outgoing messages

--- bot/main.js
+++ bot/main.js
@@ -55,6 +55,8 @@ let main = Promise.coroutine(function* (req, res) {
         timer = setTimeout(main, runTime*1000);
       }
       return processed_messages;
+    }).then(function(messages) {
+      return concatenate(messages);
     });
   }
 
@@ -62,6 +64,37 @@ let main = Promise.coroutine(function* (req, res) {
     res.end();
   }
 });
+
+const concatenate = function(messages) {
+  let tos = {};
+  for ( var i=0; i<responses.length; i++ ) {
+    let response = responses[i];
+    let to = response.to;
+    if ( !tos[to] ) {
+      tos[to] = [];
+    }
+    tos[to].push(response);
+  }
+  let newResponses = Object.keys(tos).map(function(to) {
+    return tos[to].reduce(function(output, response) {
+      if (! output) {
+        output = {
+          to: response.to,
+          from: response.from,
+          message: [response.message]
+        };
+      } else {
+        output.message.push(response.message);
+      }
+      return output;
+    }, null);
+  }).map(function(response) {
+    response.message = response.message.join('\n\n');
+    return response;
+  });
+
+  return newResponses;
+};
 
 let sendMessages = Promise.coroutine(function* (messages) {
   yield request({
