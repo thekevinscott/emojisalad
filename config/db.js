4c1ca70a282d31d4599e45563e05e29be4107f4b
Kevin Scott <kevin@scottlabsllc.com>
2015-12-30 08:30:39 -0500

    try using localhost

--- config/db.js
+++ config/db.js
@@ -8,7 +8,8 @@ var db = {
     "charset"  : "utf8mb4"
   },
   digitalOcean: {
-    "host"     : "45.55.41.73",
+    //"host"     : "45.55.41.73",
+    "host"     : "127.0.0.1",
     "user"     : "emojinaryfriend",
     "password" : "yU2ofgAizVovg9M}7Tu$d3==smAvZjqbAPagxPU7;ufYnCQGBo",
     "database" : "emojinaryfriend",
