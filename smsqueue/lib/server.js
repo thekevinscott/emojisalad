8bfd34ef06118d6d0720c496df694d976f378c3f
Kevin Scott <kevin@scottlabsllc.com>
2016-01-03 14:08:28 -0500

    alter how sms queue passes in options

--- smsqueue/lib/server.js
+++ smsqueue/lib/server.js
@@ -3,7 +3,10 @@
 const queue = require('queue');
 
 queue({
-  port: require('config/app').port,
+  options: {
+    port: require('config/app').port,
+    db: require('config/db')
+  },
   parse: require('lib/parse'),
   send: require('lib/sms')
 });
