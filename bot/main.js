073218720d7566bd90ffebc2c446edbaf200c026
Kevin Scott <kevin@scottlabsllc.com>
2016-01-02 22:58:53 -0500

    tighten up main

--- bot/main.js
+++ bot/main.js
@@ -17,46 +17,53 @@ const setTimestamp = require('lib/setTimestamp');
 
 let timer;
 
-let handle = Promise.coroutine(function* (message) {
-  return yield processMessage(message);
+let main = Promise.coroutine(function* (req, res) {
+  try {
+    console.debug('main');
+    clear();
+
+    const lastRecordedTimestamp = yield store('timestamp');
+
+    console.debug('lastRecord', lastRecordedTimestamp);
+
+    const messages = yield getMessages(lastRecordedTimestamp);
+    console.debug('messages', messages);
+
+    if ( messages.length ) {
+      console.debug('set Timestamp messages', messages);
+
+      const processed_messages = Promise.all(messages.map(processMessage));
+      console.debug('this should be an array of messages', processed_messages);
+
+      // set timestamp once we've retrieved the messages and processed them,
+      // but before we've sent them.
+      //
+      // The logic here is that, if there is some error with sending and some
+      // of the messages go out but not all of them, we don't want to 
+      // resend out potentially invalid messages, which could happen if we don't
+      // accurately record the timestamp.
+      setTimestamp(messages);
+
+      yield sendMessages(processed_messages);
+      //if ( process.env.ENVIRONMENT !== 'test' ) {
+        timer = setTimeout(main, runTime*1000);
+      //}
+    }
+
+    if ( res ) {
+      res.end();
+    }
+  } catch(err) {
+    console.error(err);
+    clear();
+    throw err;
+  }
 });
 
-let main = Promise.coroutine(function* (req, res) {
-  console.debug('main');
+function clear() {
   if ( timer ) {
     clearTimeout(timer);
   }
-
-  let lastRecordedTimestamp = yield store('timestamp');
-  let previousRunTime = (new Date()).getTime()/1000 - runTime;
-  if ( lastRecordedTimestamp < previousRunTime ) {
-    lastRecordedTimestamp = previousRunTime;
-  }
-
-  console.debug('lastRecord', lastRecordedTimestamp);
-
-  const messages = yield getMessages(lastRecordedTimestamp);
-  console.debug('messages', messages);
-
-  if ( messages.length ) {
-    setTimestamp(messages);
-
-    console.debug('messages from mongo', messages);
-
-    let processed_messages = Promise.all(messages.map(processMessage));
-    processed_messages = _.flatten(processed_messages)
-    console.debug('this should be an array of messages', processed_messages);
-    yield sendMessages(processed_messages);
-    if ( process.env.ENVIRONMENT !== 'test' ) {
-      timer = setTimeout(main, runTime*1000);
-    }
-  }
-
-  if ( res ) {
-    res.end();
-  }
-});
-
-
+}
 
 module.exports = main;
