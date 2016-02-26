ff5f53f8eef428be7f11739b7ecedce04dc7d0f0
Kevin Scott <kevin@scottlabsllc.com>
2016-02-25 20:49:33 -0500

    more debuging

--- bot/routes/create-user.js
+++ bot/routes/create-user.js
@@ -5,7 +5,9 @@ const User = require('models/user');
 const _ = require('lodash');
 
 module.exports = (from, input, to) => {
+  console.info('lets create that user');
   return User.create({ from: from }).then((user) => {
+    console.info('created user', user);
     let player = _.extend(user, { to: to });
     return [{
       key: 'intro',
