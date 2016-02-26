6a5c3a2286265dfa8ec4e49f4e59dda2154b11f9
Kevin Scott <kevin@scottlabsllc.com>
2016-02-25 19:56:55 -0500

    use local host for db

--- bot/config/database/production.js
+++ bot/config/database/production.js
@@ -1,5 +1,6 @@
 module.exports = {
-  "host"     : "45.55.41.73",
+  //"host"     : "45.55.41.73",
+  "host"     : "127.0.0.1",
   "user"     : "emojinaryfriend",
   "password" : "yU2ofgAizVovg9M}7Tu$d3==smAvZjqbAPagxPU7;ufYnCQGBo",
   "database" : "emojinaryfriend",
