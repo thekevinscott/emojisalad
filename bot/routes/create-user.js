6bb0a351479cc8d1166f584f3e6e2cf4bb30f661
Kevin Scott <kevin@scottlabsllc.com>
2016-02-28 16:31:18 -0500

    throw error if cant create a user

--- bot/routes/create-user.js
+++ bot/routes/create-user.js
@@ -6,12 +6,18 @@ const _ = require('lodash');
 
 module.exports = (from, input, to) => {
   console.info('lets create that user');
-  return User.create({ from: from }).then((user) => {
-    console.info('created user', user);
-    let player = _.extend(user, { to: to });
-    return [{
-      key: 'intro',
-      player: player
-    }];
+  return User.create({ from: from }).then((response) => {
+    if ( response.error ) {
+      console.error('Error creating user', response);
+      throw new Error('Error creating user');
+    } else {
+      const user = response;
+      console.info('created user', user);
+      let player = _.extend(user, { to: to });
+      return [{
+        key: 'intro',
+        player: player
+      }];
+    }
   });
 };
