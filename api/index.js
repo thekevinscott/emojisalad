150ab35928ef08f373f54a58229006681856a256
Kevin Scott <kevin@scottlabsllc.com>
2016-02-28 16:42:49 -0500

    lots more logging

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
 'use strict';
+console.info('1');
 if ( ! process.env.ENVIRONMENT ) {
   throw "No environment provided";
 }
+console.info('2');
 require('babel-core/register')
+console.info('3');
 require('app-module-path').addPath(__dirname);
+console.info('4');
 
 require('../shared/scaffolding');
+console.info('5');
 require('./server'); 
+console.info('6');
