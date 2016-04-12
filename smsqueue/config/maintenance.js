9ccc99258cb5521510c34190746fdfee769fe33a
Kevin Scott <kevin@scottlabsllc.com>
2016-04-12 19:55:26 -0400

    add google voice to maintenance

--- smsqueue/config/maintenance.js
+++ smsqueue/config/maintenance.js
@@ -1,8 +1,8 @@
 const DOWN_FOR_MAINTENANCE = true;
 
 const whitelist = [
-  '+18604608183'/*, // kevin
-  '+15125222183', // kevin's google voice
+  '+18604608183', // kevin
+  '+15125222183'/*, // kevin's google voice
   '+14126382398', // ari
   '+14152975657', // michelle
   '+19175094334' // beth
