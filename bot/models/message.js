69d4c78d04b284c3b307389022468856b8c1a2ac
Kevin Scott <kevin@scottlabsllc.com>
2016-04-30 17:21:26 -0400

    Remove async

--- bot/models/message.js
+++ bot/models/message.js
@@ -7,7 +7,7 @@ const db = require('db');
 //const config = require('../../config/twilio')[process.env.ENVIRONMENT];
 
 const Message = {
-  get: Promise.coroutine(function* (key, options) {
+  get: (key, options) => {
     if ( ! options ) {
       options = [];
     }
@@ -20,44 +20,45 @@ const Message = {
                   .from('messages')
                   .where('`key` IN ?',key);
 
-    const messages = yield db.query(query);
-    if ( messages.length ) {
-      // expects an options argument
-      // that is an object containing keys
-      // that correspond to a message's key.
-      //
-      // {
-      //  intro_4: [
-      //    '8604601234'
-      //  ]
-      // }
-      return messages.map((obj) => {
-        if ( obj.message ) {
-          // this is coming from an old database
-          obj.body = obj.message;
-          delete obj.message;
-        }
-        if ( options[obj.key] ) {
-          if ( _.isArray(options[obj.key]) ) {
-            obj.body = sprintf.apply(null, [obj.body].concat(options[obj.key]));
-          } else {
-            //let old = { game: { round: {submitter: {nickname: 'foobar'} } } };
-            obj.body = sprintf(obj.body, options[obj.key]);
+    return db.query(query).then((messages) => {
+      if ( messages.length ) {
+        // expects an options argument
+        // that is an object containing keys
+        // that correspond to a message's key.
+        //
+        // {
+        //  intro_4: [
+        //    '8604601234'
+        //  ]
+        // }
+        return messages.map((obj) => {
+          if ( obj.message ) {
+            // this is coming from an old database
+            obj.body = obj.message;
+            delete obj.message;
           }
-        }
-        return {
-          id: obj.id,
-          key: obj.key,
-          //name: obj.name
-          body: obj.body
-        };
-      });
+          if ( options[obj.key] ) {
+            if ( _.isArray(options[obj.key]) ) {
+              obj.body = sprintf.apply(null, [obj.body].concat(options[obj.key]));
+            } else {
+              //let old = { game: { round: {submitter: {nickname: 'foobar'} } } };
+              obj.body = sprintf(obj.body, options[obj.key]);
+            }
+          }
+          return {
+            id: obj.id,
+            key: obj.key,
+            //name: obj.name
+            body: obj.body
+          };
+        });
 
-    } else {
-      throw "No messages found for key " + key;
-    }
-  }),
-  parse: Promise.coroutine(function* (responses, original_message = {}) {
+      } else {
+        throw "No messages found for key " + key;
+      }
+    });
+  },
+  parse: (responses, original_message = {}) => {
     if ( responses && responses.length ) {
       const messages = {};
       const options = {};
@@ -67,54 +68,55 @@ const Message = {
         }
       });
 
-      const rows = yield Message.get(responses.map((response) => {
+      return Message.get(responses.map((response) => {
         if ( ! response.key ) {
           throw new Error("Every response must have a key: " + JSON.stringify(response));
         }
         return response.key;
-      }), options);
+      }), options).then((rows) => {
 
-      rows.map((row) => {
-        messages[row.key] = row.body;
-      });
+        rows.map((row) => {
+          messages[row.key] = row.body;
+        });
 
-      return responses.map((response) => {
-        let from;
+        return responses.map((response) => {
+          let from;
 
-        //console.info('response player', response);
-        if ( response.player.from ) {
-          from = response.player.from;
-        } else if ( response.player.user.from ) {
-          from = response.player.user.from;
-        }
-        const to = response.player.to;
+          //console.info('response player', response);
+          if ( response.player.from ) {
+            from = response.player.from;
+          } else if ( response.player.user.from ) {
+            from = response.player.user.from;
+          }
+          const to = response.player.to;
 
-        if ( ! to ) {
-          console.error(response);
-          throw "Missing to field in Message.parse";
-        }
+          if ( ! to ) {
+            console.error(response);
+            throw "Missing to field in Message.parse";
+          }
 
-        if ( ! from ) {
-          console.error(response);
-          throw "Missing from field in Message.parse";
-        }
+          if ( ! from ) {
+            console.error(response);
+            throw "Missing from field in Message.parse";
+          }
 
-        // The 'to' and 'from fields get flipped.
-        // i.e., a user's number, which until now
-        // was the 'from' field, is now the number
-        // we send to, aka the 'to' field.
-        return _.assign({
-          body: messages[response.key],
-          to: from,
-          from: to,
-          protocol: response.player.protocol || original_message.protocol,
-          initiated_id: original_message.id
-        }, response);
+          // The 'to' and 'from fields get flipped.
+          // i.e., a user's number, which until now
+          // was the 'from' field, is now the number
+          // we send to, aka the 'to' field.
+          return _.assign({
+            body: messages[response.key],
+            to: from,
+            from: to,
+            protocol: response.player.protocol || original_message.protocol,
+            initiated_id: original_message.id
+          }, response);
+        });
       });
     } else {
       return [];
     }
-  })
+  }
 };
 
 module.exports = Message;
