a6bfe405d2a17205d6f95bfb5621ec2b3b6af57e
Kevin Scott <kevin@scottlabsllc.com>
2016-07-24 13:57:43 -0400

    Add option to use production or development

--- EmojiSalad/app/utils/Api/config.js
+++ EmojiSalad/app/utils/Api/config.js
@@ -1,3 +1,22 @@
 export const NETWORK_TIMEOUT = 5000;
-export const API_HOST = 'localhost';
-export const API_PORT = '5008';
+const ENVIRONMENT = 'production';
+
+function getAPI(environment) {
+  if (environment === 'production') {
+    return {
+      API_HOST: '45.55.41.73',
+      API_PORT: '5012',
+    };
+  }
+  return {
+    API_HOST: 'localhost',
+    API_PORT: '5008',
+  };
+}
+const {
+  API_HOST: h,
+  API_PORT: p,
+} = getAPI(ENVIRONMENT);
+
+export const API_HOST = h;
+export const API_PORT = p;
