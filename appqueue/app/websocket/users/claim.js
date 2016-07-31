447aedc0840fd9efaaf20a8369d01713aac2084e
Kevin Scott <kevin@scottlabsllc.com>
2016-07-31 09:53:24 -0400

    add additional logging

--- appqueue/app/websocket/users/claim.js
+++ appqueue/app/websocket/users/claim.js
@@ -8,16 +8,19 @@ export default function claim(ws, payload) {
   console.info('claim', text, device);
 
   return parsePhone(text).then(phone => {
+    console.info('phone parsed', phone);
     if (!phone) {
       throw new Error('Invalid phone number');
     }
 
     return getUsers(phone).then(users => {
+      console.info('users got', users);
       if (users.length === 0) {
         throw new Error(`No users found for ${text}`);
       }
       return users.shift();
     }).then(user => {
+      console.info('now, upadte users');
       updateUser(ws, user, {
         device,
         phone,
