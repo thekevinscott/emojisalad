61f7ae7fbce834fe0494c3d41c14a1fe22fe43f3
Kevin Scott <kevin@scottlabsllc.com>
2016-08-02 18:40:02 -0400

    update pass to fix bug

--- bot/routes/game/pass.js
+++ bot/routes/game/pass.js
@@ -6,8 +6,11 @@ const rule = require('config/rule');
 const Round = require('models/round');
 const newRound = require('./new_round');
 const Game = require('models/game');
+const Timer = require('models/timer');
 
 module.exports = (game, player, input) => {
+  Timer.clear('submission', game.id);
+  Timer.clear('submission-2', game.id);
   return require('./say')(game, player, input).then((messages) => {
     return Round.create(game).then((round) => {
       return messages.concat(game.players.map((game_player) => {
