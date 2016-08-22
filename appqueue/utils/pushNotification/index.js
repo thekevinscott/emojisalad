c6a136260bd1a4ac7528cd79d6672e7439f6c626
Kevin Scott <kevin@scottlabsllc.com>
2016-08-21 20:17:29 -0400

    More logging on appqueue

--- appqueue/utils/pushNotification/index.js
+++ appqueue/utils/pushNotification/index.js
@@ -23,7 +23,9 @@ const getCachedDevice = userKey => {
 
 export default function sendNotification(userKey, body, options = {}) {
   getCachedDevice(userKey).then(device => {
+    console.info('got device', device);
     const note = getNotification(body, options);
+    console.info('got note', note);
     connection.pushNotification(note, device);
     console.info(`sent ${note.alert.body} on ${device}`);
   });
