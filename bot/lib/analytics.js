d501652e2013871108c8e276fa5446224f6a8edd
Kevin Scott <kevin@scottlabsllc.com>
2016-10-14 18:32:19 -0400

    Yeah lets try this now

--- bot/lib/analytics.js
+++ bot/lib/analytics.js
@@ -1,6 +1,8 @@
+import fetchFromService from './utils/fetchFromService';
 const token = "95fd9ab140918c0f5e8bef7ba00680fb";
 const Promise = require('bluebird');
 const request = Promise.promisify(require('request'));
+const registry = require('microservice-registry');
 
 function getOptions(user_id, text, platform, who_speaks) {
   return {
@@ -19,7 +21,7 @@ function getOptions(user_id, text, platform, who_speaks) {
 function outgoing(messages) {
   return Promise.all(messages.map(message => {
     return request(getOptions(
-      message.player.to,
+      message.player.key,
       message.body,
       message.player.protocol,
       'bot'
@@ -27,14 +29,32 @@ function outgoing(messages) {
   }));
 }
 
+function getUser(phone) {
+  return fetchFromService({
+    service: 'api',
+    route: 'users.get',
+    options: {
+      from: phone,
+    },
+  });
+}
+
 function incoming(messages) {
   return Promise.all(messages.map(message => {
-    return request(getOptions(
-      message.from,
-      message.body,
-      message.protocol,
-      'user'
-    ));
+    return getUser(message.from).then(users => {
+      if (users.length) {
+        return users.shift();
+      }
+
+      throw new Error(`No user found for: ${message.from}`);
+    }).then(user => {
+      return request(getOptions(
+        user.key,
+        message.body,
+        message.protocol,
+        'user'
+      ));
+    });
   }));
 }
 
---
+++ bot/lib/utils/fetchFromService.js
@@ -0,0 +1,68 @@
+import registry from 'microservice-registry';
+import fetch from './fetch';
+
+function getApi(service, route) {
+  if (route.length) {
+    return getApi(service[route[0]], route.slice(1));
+  }
+  return service;
+}
+
+function parseEndpoint(url, params = {}) {
+  let parsedUrl = url;
+
+  Object.keys(params).forEach(k => {
+    const key = `:${k}`;
+    parsedUrl = parsedUrl.split(key).join(params[k]);
+  });
+
+  return parsedUrl;
+}
+
+function getRequestedRoute(service, route, routeParams) {
+  const serviceEndpoint = (registry.get(service) || {}).api;
+  if (serviceEndpoint) {
+    const {
+      endpoint,
+      method,
+    } = getApi(serviceEndpoint, route.split('.'));
+
+    return {
+      url: parseEndpoint(endpoint, routeParams),
+      method,
+    };
+  }
+
+  throw new Error(`No Endpoint for ${service}`);
+}
+
+export default function fetchFromService({
+  service,
+  route,
+  routeParams,
+  options,
+}) {
+  return new Promise((resolve, reject) => {
+    try {
+      const {
+        url,
+        method,
+      } = getRequestedRoute(service, route, routeParams);
+
+      if (!url) {
+        reject(`URL does not exist for route: ${route}`);
+      }
+
+      const payload = {
+        method,
+        ...options,
+      };
+
+      //console.info('fetching with', url, payload);
+      return fetch(url, payload).then(resolve).catch(resolve);
+    } catch (err) {
+      reject(err.message);
+    }
+  });
+}
+
