cf9027b2bd7ceed1c19e5bf1a6e60823633db1f7
Kevin Scott <kevin@scottlabsllc.com>
2016-11-05 16:44:04 -0700

    Better io

--- nexmoqueue/lib/jungleParse.js
+++ nexmoqueue/lib/jungleParse.js
@@ -5,21 +5,17 @@ const websocket = require('./websocket');
 
 let callback = () => {};
 
+let io;
+
 const timer = setInterval(() => {
-  const io = websocket();
+  io = websocket();
   if (io) {
     console.log('io now exists');
     clearInterval(timer);
     io.on('connection', (socket) => {
       console.log('got connection');
-      const callback = (number, message) => {
-        console.log('broadcast', number, message);
-        socket.broadcast.emit('message', {
-          number,
-          message,
-        });
-      };
     });
+
   }
 }, 50);
 
@@ -44,7 +40,13 @@ module.exports = function jungleParse({
 
   console.log(query.toString());
   return db.query(query).then((rows) => {
-    callback(number, message);
+    console.log('broadcast', number, message);
+    if (io) {
+      io.emit('message', {
+        number,
+        message,
+      });
+    }
     return rows;
   });
 }
