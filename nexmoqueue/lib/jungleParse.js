b2ef2d84f008d79184518c79547e77d5db5f40be
Kevin Scott <kevin@scottlabsllc.com>
2016-11-05 13:03:21 -0700

    Save jungle messages

--- nexmoqueue/lib/jungleParse.js
+++ nexmoqueue/lib/jungleParse.js
@@ -1,3 +1,26 @@
-export default function jungleParse(params) {
-  console.info('incoming params for da jungle', params);
+import phoneFormatter from 'node-phone-formatter';
+import db from '../db';
+import squel from 'squel';
+
+export default function jungleParse({
+  text: message,
+  msisdn,
+}) {
+  const number = phoneFormatter.format(msisdn, "(NNN) NNN-NNNN");
+  console.info('incoming params for da jungle', message, number);
+
+  const query = squel
+  .insert({
+    autoEscapeFieldNames: true,
+  })
+  .into('jungle_messages')
+  .setFields({
+    number,
+    message,
+    created: squel.fval('NOW(3)'),
+  });
+
+  return db.query(query).then((rows) => {
+    return rows;
+  });
 }
