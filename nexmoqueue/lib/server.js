352778c1d4516aef405f3722bacda48d94b13401
Kevin Scott <kevin@scottlabsllc.com>
2016-11-05 16:19:17 -0700

    Allow all

--- nexmoqueue/lib/server.js
+++ nexmoqueue/lib/server.js
@@ -57,10 +57,10 @@ const whitelist = [
   'http://nexmo.emojisalad.com',
 ];
 app.use(cors({
-  origin: (origin, callback) => {
-    const originIsWhitelisted = whitelist.indexOf(origin) !== -1;
-    callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
-  },
+  //origin: (origin, callback) => {
+    //const originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+    //callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
+  //},
 }));
 
 const phone = require('lib/phone');
