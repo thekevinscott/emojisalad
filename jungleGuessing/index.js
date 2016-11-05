831e06444263ac88c8fb9c391174a5a1f33dd4e7
Kevin Scott <kevin@scottlabsllc.com>
2016-11-05 13:46:33 -0700

    Try again

--- jungleGuessing/index.js
+++ jungleGuessing/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.use(express.static(`${__dirname}/dist`));
 
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html');
+  res.sendFile(path.join(`${__dirname}/index.html`));
 });
 
 app.listen(port, () => {
