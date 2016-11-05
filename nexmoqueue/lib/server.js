4d11f9721f4a6a8e9953987e223729732afa4875
Kevin Scott <kevin@scottlabsllc.com>
2016-11-05 15:06:06 -0700

    ADd cors

--- nexmoqueue/lib/server.js
+++ nexmoqueue/lib/server.js
@@ -3,6 +3,7 @@
 const port = process.env.PORT;
 const ENVIRONMENT = process.env.ENVIRONMENT || 'development';
 
+const cors = require('cors');
 const endpoint = `http://localhost:${require('config/app').port}/`;
 
 console.info('endpoint for nexmo queue', endpoint);
@@ -49,23 +50,8 @@ const app = require('queue')({
   },
 });
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Origin', 'http://jungle.emojisalad.com');
-  res.setHeader('Access-Control-Allow-Origin', 'https://jungle.emojisalad.com');
 
-  res.setHeader("Access-Control-Allow-Origin: *");
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-});
+app.use(cors());
 
 const phone = require('lib/phone');
 app.get('/phone', (req, res) => {
