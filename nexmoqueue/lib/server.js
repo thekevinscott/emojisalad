9dc8b438009440e14cd1afd0e5dba61d33b425ad
Kevin Scott <kevin@scottlabsllc.com>
2016-11-05 15:00:51 -0700

    Try to enable cors

--- nexmoqueue/lib/server.js
+++ nexmoqueue/lib/server.js
@@ -68,3 +68,19 @@ app.get('/', (req, res) => {
 
 app.post('/delivery', require('./delivery'));
 app.get('/jungle', require('./jungle'));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://jungle.emojisalad.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://jungle.emojisalad.com');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+});
