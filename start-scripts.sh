3b77bada346e579ce5fce7db058e5ea1e794e9ea
Kevin Scott <kevin@scottlabsllc.com>
2016-11-05 09:27:52 -0700

    Add start script

---
+++ start-scripts.sh
@@ -0,0 +1,26 @@
+ #!/bin/bash          
+ echo Hello World   
+
+ cd /var/www/appqueue
+ pm2 start app.json
+
+ cd /var/www/mailqueue
+ pm2 start app.json
+
+ cd /var/www/nexmoqueue
+ pm2 start app.json
+
+ cd /var/www/smsqueue
+ pm2 start app.json
+
+ cd /var/www/webqueue
+ pm2 start app.json
+
+ cd /var/www/web
+ pm2 start app.json
+
+ cd /var/www/api
+ pm2 start app.json
+
+ cd /var/www/bot
+ pm2 start app.json
