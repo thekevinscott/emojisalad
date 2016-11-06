7fba59dc500440baa4284a1a4983ee8a166ef743
Kevin Scott <kevin@scottlabsllc.com>
2016-11-06 11:49:32 -0800

    default to a string

--- bot/routes/challenge/index.js
+++ bot/routes/challenge/index.js
@@ -3,10 +3,11 @@ import guess from './guess';
 import rule from 'config/rule';
 
 module.exports = (params) => {
-  if ( rule('clue').test(params.message) ) {
+  const message = params.message || '';
+  if ( rule('clue').test(message) ) {
     console.info('challenge clue');
     return require('./clue')(params);
-  } else if ( rule('help').test(params.message) ) {
+  } else if ( rule('help').test(message) ) {
     console.info('game help');
     return require('./help')(params);
   }
