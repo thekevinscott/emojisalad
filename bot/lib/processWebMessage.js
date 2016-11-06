6e72378844bf446e8471d0b48b127b7fb502a2f9
Kevin Scott <kevin@scottlabsllc.com>
2016-11-06 12:45:43 -0800

    Feed in the correct router

--- bot/lib/processWebMessage.js
+++ bot/lib/processWebMessage.js
@@ -5,7 +5,10 @@ const Message = require('models/message');
 const concatenate = require('lib/concatenateMessages');
 
 module.exports = (entry) => {
-  return router(entry.phone).then(responses => {
+  return router({
+    from: entry.phone,
+    protocol: 'sms',
+  }).then(responses => {
     if ( responses && _.isArray(responses) && responses.length ) {
       return Message.parse(responses, {});
     }
