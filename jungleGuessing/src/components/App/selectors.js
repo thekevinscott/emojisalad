ad642bf0fb3210f718ca5d3f5485f882d12fc989
Kevin Scott <kevin@scottlabsllc.com>
2016-11-06 08:48:42 -0800

    Turn off faker

--- jungleGuessing/src/components/App/selectors.js
+++ jungleGuessing/src/components/App/selectors.js
@@ -13,7 +13,7 @@ const isCorrect = ({
   return distance < 5;
 };
 
-const validGuess = (guess, obj, phrase) => {
+const validGuess = (guess, obj, phrase, isPossibleToWin) => {
   if (!obj[guess.number]) {
     return true;
   }
@@ -21,7 +21,7 @@ const validGuess = (guess, obj, phrase) => {
   const currentDate = new Date(obj[guess.number].created);
   const newDate = new Date(guess.created);
 
-  if (currentDate < newDate && !isCorrect(obj[guess.number], phrase)) {
+  if (isPossibleToWin && currentDate < newDate && !isCorrect(obj[guess.number], phrase)) {
     return true;
   }
 
@@ -36,7 +36,7 @@ const selectGuesses = (guesses, {
   const guessObj = guesses.filter(guess => {
     return guess.phraseId === phraseId;
   }).reduce((obj, guess) => {
-    if (validGuess(guess, obj, phrase)) {
+    if (validGuess(guess, obj, phrase, true)) {
       const correct = !foundCorrect && isCorrect(guess, phrase);
 
       if (correct) {
