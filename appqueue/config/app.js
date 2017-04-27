98d96190c5cdbaa6229701645e2bfcf065ac4764
Kevin Scott <tech+temp@ledgerx.com>
2017-04-27 16:14:51 -0400

    Update push notification sending

--- appqueue/config/app.js
+++ appqueue/config/app.js
@@ -1,7 +1,11 @@
 module.exports = {
-  PUSHCITY: {
-    URL: 'https://api.pushcityapp.com/api/v1/',
-    API_KEY: '94065890d025a5989ddf0b67862d62739e77ef64af0c9e6050a46a9796380d43',
+  //PUSHCITY: {
+    //URL: 'https://api.pushcityapp.com/api/v1/',
+    //API_KEY: '94065890d025a5989ddf0b67862d62739e77ef64af0c9e6050a46a9796380d43',
+  //},
+  ONE_SIGNAL: {
+    API_KEY: 'OWE3MTY3MDctNmQ3NS00MWY3LTkzZmEtM2JhYTI3NjNjM2Fk',
+    APP_ID: 'f5e80a38-fcc6-4f7c-bfcf-fb82da346390',
   },
   ENVIRONMENT: process.env.ENVIRONMENT || 'development',
   HOST: 'localhost',
--- appqueue/utils/pushNotification/index.js
+++ appqueue/utils/pushNotification/index.js
@@ -1,42 +1,41 @@
-/*
-  var body = {
-    apiKey: apiKey,
-    userID: userID,
-    badge: badge || 0,
-    notification: {
-      alert: note,
-    }
-  };
-
-  $.post('/api/v1/notify', body, function(response) {
-    $('#results').append(JSON.stringify(response) + '<br />');
-  });
-}
-*/
 import fetch from '../fetch';
 
 import {
-  PUSHCITY,
+  ONE_SIGNAL,
 } from '../../config/app';
 
+//curl --include --request POST
+     //--data-binary "{\"app_id\": \"f5e80a38-fcc6-4f7c-bfcf-fb82da346390\",
+//\"contents\": {\"en\": \"Big Poppa\"},
+//\"include_player_ids\": [\"a26ce097-5e75-4900-bf34-4e0db40e7a65\" ]}" \
 export default function pushNotification(userKey, gameKey, body, options = {}) {
-  const url = `${PUSHCITY.URL}notify`;
+  //const url = `${PUSHCITY.URL}notify`;
   const params = {
-    apiKey: PUSHCITY.API_KEY,
-    userID: userKey,
-    notification: {
-      alert: body,
-      badge: options.badge || 0,
-      view: `games/${gameKey}`,
-    },
-  };
+    app_id: ONE_SIGNAL.APP_ID,
+    contents: {"en": "English Message"},
+    included_segments
+    //apiKey: PUSHCITY.API_KEY,
+    //userID: userKey,
+    //notification: {
+      //alert: body,
+      //badge: options.badge || 0,
+      //view: `games/${gameKey}`,
+    //},
+  //};
   console.info('this is a push notification sending', url, params);
   //fetch(`${PUSHCITY.URL}`).then(response => {
     //console.log('got the response from root', response);
   //});
+  
+  const url = 'https://onesignal.com/api/v1/notifications';
+  
   return fetch(url, {
     method: 'post',
-    body: params,
+    body: JSON.stringify(params),
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8', 
+      'Authorization': `Basic ${ONE_SIGNAL.API_KEY}`,
+    },
   }).then(response => {
     console.log('got response back from', url, response);
     return response;
