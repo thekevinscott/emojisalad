7e5a2b8e3f06cc56321f384794e7d053b5ab5f38
Kevin Scott <tech+temp@ledgerx.com>
2017-05-07 17:51:03 -0400

    Only fetch unused invites

--- appqueue/app/rest/lib/translate/fetchPlayerTo.js
+++ appqueue/app/rest/lib/translate/fetchPlayerTo.js
@@ -30,6 +30,7 @@ export default function fetchPlayerTo(userKey, gameKey) {
         options: {
           invited_from_key: userKey,
           game_key: gameKey,
+          used: 0,
         },
       }).then(invites => {
         console.info('invites back', invites);
